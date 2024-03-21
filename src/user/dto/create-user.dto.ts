c2346b38152bf8c3c9ad93aa1a54b89d66d620d4
fresemin <fresemin@gmail.com>
2024-03-21 23:15:53 +0300

    feat: create user basics

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,20 @@
-export class CreateUserDto {}
+import { IsRepeated } from '@common/decorators';
+import { IsEmail, IsString, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+    @IsString()
+    @MinLength(3)
+    name: string;
+
+    @IsEmail()
+    email: string;
+
+    @IsString()
+    @MinLength(8)
+    password: string;
+
+    @IsString()
+    @MinLength(8)
+    @IsRepeated('password')
+    retypedPassword: string;
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(
+        @InjectRepository(User)
+        private readonly userRepository: Repository<User>,
+    ) {}
+
+    async create(createUserDto: CreateUserDto): Promise<User> {
+        const newUser = await this.userRepository.create(createUserDto);
+
+        return await this.userRepository.save(newUser);
+    }
+}
