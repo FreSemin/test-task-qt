11ac1bc90f2874c30bf7c23af9fc25762f87d28a
fresemin <fresemin@gmail.com>
2024-03-21 19:50:58 +0300

    feat: set up validation

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { APP_DOCKER_PORT, APP_LISTEN_DOCKER_PORT, APP_LISTEN_PORT, APP_PORT, APP_PREFIX } from '@common/constants';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
@@ -9,6 +10,8 @@ async function bootstrap() {
     const configService = app.get(ConfigService);
 
     app.setGlobalPrefix(configService.get('APP_PREFIX', APP_PREFIX));
+
+    app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
     const port = configService.get<number>('APP_PORT', APP_PORT);
 
