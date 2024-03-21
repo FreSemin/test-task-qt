bad280ee565cdba402bc41ec1fa1a3ce3fc2eae5
fresemin <fresemin@gmail.com>
2024-03-21 15:52:14 +0300

    feat: use env vars in main

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { APP_DOCKER_PORT, APP_LISTEN_DOCKER_PORT, APP_LISTEN_PORT, APP_PORT } from '@common/constants';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    await app.listen(3000);
+
+    const configService = app.get(ConfigService);
+
+    const port = configService.get<number>('APP_PORT', APP_PORT);
+
+    const dockerPort = configService.get<number>('DOCKER_API_PORT', APP_DOCKER_PORT);
+
+    await app.listen(port).then(() => {
+        console.log(APP_LISTEN_PORT(port));
+        console.log(APP_LISTEN_DOCKER_PORT(dockerPort));
+    });
 }
 bootstrap();
