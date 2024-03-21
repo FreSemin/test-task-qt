b7abc8ff4b8ba2ce2d5e83fa244f41351a4bb66d
fresemin <fresemin@gmail.com>
2024-03-21 15:51:53 +0300

    feat: app constants

---
+++ libs/common/src/constants/app.constants.ts
@@ -0,0 +1,7 @@
+export const APP_PORT = 4000;
+
+export const APP_DOCKER_PORT = 4000;
+
+export const APP_LISTEN_PORT = (port: number) => `APP listening on port ${port}`;
+
+export const APP_LISTEN_DOCKER_PORT = (port: number) => `APP docker external port ${port}`;
