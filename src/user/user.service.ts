0256b5cbd357baf099522d2e025a5ee153c8fc35
fresemin <fresemin@gmail.com>
2024-03-22 14:53:46 +0300

    feat: hash password

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,23 +3,35 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UserEntity } from './entities/user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { EmailTakenError } from '@common/utils';
+import { genSalt, hash } from 'bcrypt';
 
 @Injectable()
 export class UserService {
     constructor(
         @InjectRepository(UserEntity)
         private readonly userRepository: Repository<UserEntity>,
+        private readonly configService: ConfigService,
     ) {}
+
+    private async hashPassword(password: string): Promise<string> {
+        const salt = await genSalt(Number(this.configService.get<number>('CRYPT_SALT', 10)));
+
+        return await hash(password, salt);
+    }
 
     async create(createUserDto: CreateUserDto): Promise<UserEntity> {
         const existingUser = await this.findOneByEmail(createUserDto.email);
 
         if (existingUser) {
-            throw new EmailTakenError();
+            throw new EmailTakenError(EMAIL_TAKEN_TEXT(createUserDto.email));
         }
 
-        const newUser = await this.userRepository.create(createUserDto);
+        const hashedPassword = await this.hashPassword(createUserDto.password);
+
+        const newUser = await this.userRepository.create({
+            ...createUserDto,
+            password: hashedPassword,
+        });
 
         return await this.userRepository.save(newUser);
     }
