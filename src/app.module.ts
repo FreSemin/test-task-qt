469b64c3472e8c470efc8a2913ea37a171c9369f
fresemin <fresemin@gmail.com>
2024-03-22 15:19:01 +0300

    feat: init auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { DatabaseModule } from '@database/database.module';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from '@user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
     imports: [
@@ -10,6 +11,7 @@ import { UserModule } from '@user/user.module';
         }),
         DatabaseModule,
         UserModule,
+        AuthModule,
     ],
     controllers: [],
     providers: [],
