0d1fb817569261cf77c3d50aa16c9a7f09bd6db9
fresemin <fresemin@gmail.com>
2024-03-23 13:57:01 +0300

    feat: basic redis service

--- src/redis/redis.module.ts
+++ src/redis/redis.module.ts
@@ -1,7 +1,24 @@
 import { Module } from '@nestjs/common';
 import { RedisService } from './redis.service';
+import { CacheModule } from '@nestjs/cache-manager';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { RedisClientOptions } from 'redis';
+import { redisStore } from 'cache-manager-redis-yet';
 
 @Module({
+    imports: [
+        CacheModule.registerAsync<RedisClientOptions>({
+            imports: [ConfigModule],
+            useFactory: async (configService: ConfigService) => ({
+                store: await redisStore({
+                    url: configService.get('REDIS_URL'),
+                    ttl: configService.get('REDIS_TTL'),
+                }),
+            }),
+            inject: [ConfigService],
+        }),
+    ],
     providers: [RedisService],
+    exports: [RedisService],
 })
 export class RedisModule {}
