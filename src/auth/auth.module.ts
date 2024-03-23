1199c82ff0d29bac8bd3532005d0511fd2f4d2e5
fresemin <fresemin@gmail.com>
2024-03-24 02:51:46 +0300

    feat: auth jwt configuration

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { UserModule } from '@user/user.module';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { RedisModule } from '@redis/redis.module';
 
 @Module({
+    imports: [
+        UserModule,
+        JwtModule.registerAsync({
+            imports: [ConfigModule],
+            useFactory: async (configService: ConfigService) => ({
+                secret: configService.get('JWT_SECRET_KEY'),
+                signOptions: {
+                    // TODO: add default value
+                    expiresIn: configService.get('TOKEN_EXPIRE_TIME'),
+                },
+            }),
+            inject: [ConfigService],
+        }),
+        RedisModule,
+    ],
     controllers: [AuthController],
     providers: [AuthService],
 })
