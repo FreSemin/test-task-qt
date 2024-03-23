6abb9c67baa4103facbf8f6c7f1338e58a754630
fresemin <fresemin@gmail.com>
2024-03-24 02:52:38 +0300

    feat: reg login refresh auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,99 @@
-import { Controller } from '@nestjs/common';
+import {
+    Body,
+    ClassSerializerInterceptor,
+    ConflictException,
+    Controller,
+    Get,
+    HttpStatus,
+    NotFoundException,
+    Post,
+    Res,
+    UnauthorizedException,
+    UseInterceptors,
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LoginDto, RegisterDto } from './dto';
+import { EmailTakenError, EntityNotFoundError, InvalidCredentialsError } from '@common/utils';
+import { Tokens } from './interfaces';
+import { Response } from 'express';
+import { ConfigService } from '@nestjs/config';
+import { Cookie, UserAgent } from '@common/decorators';
 
+const REFRESH_TOKEN = 'refreshtoken';
+
+@UseInterceptors(ClassSerializerInterceptor)
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(
+        private readonly authService: AuthService,
+        private readonly configService: ConfigService,
+    ) {}
+
+    private setRefreshTokenToCookies(tokens: Tokens, res: Response) {
+        if (!tokens) {
+            throw new UnauthorizedException();
+        }
+
+        // TODO: add Cookie name to constants
+        res.cookie(REFRESH_TOKEN, tokens.refreshToken.token, {
+            httpOnly: true,
+            sameSite: 'lax',
+            expires: new Date(tokens.refreshToken.exp),
+            secure: this.configService.get('NODE_ENV', 'development') === 'production',
+            path: '/',
+        });
+
+        res.status(HttpStatus.CREATED).json({ accessToken: tokens.accessToken });
+    }
+
+    @Post('reg')
+    async reg(@Body() registerDto: RegisterDto) {
+        try {
+            return await this.authService.reg(registerDto);
+        } catch (err) {
+            if (err instanceof EmailTakenError) {
+                throw new ConflictException(err.message);
+            }
+
+            throw err;
+        }
+    }
+
+    @Post('login')
+    async login(@Body() loginDto: LoginDto, @Res() res: Response, @UserAgent() agent: string) {
+        try {
+            const tokens = await this.authService.login(loginDto, agent);
+
+            this.setRefreshTokenToCookies(tokens, res);
+        } catch (err) {
+            if (err instanceof EntityNotFoundError) {
+                throw new NotFoundException(err.message);
+            }
+
+            if (err instanceof InvalidCredentialsError) {
+                throw new UnauthorizedException(err.message);
+            }
+
+            throw err;
+        }
+    }
+
+    @Get('refresh')
+    async refreshTokens(@Cookie(REFRESH_TOKEN) refreshToken: string, @Res() res: Response, @UserAgent() agent: string) {
+        try {
+            if (!refreshToken) {
+                throw new UnauthorizedException();
+            }
+
+            const tokens = await this.authService.refreshTokens(refreshToken, agent);
+
+            this.setRefreshTokenToCookies(tokens, res);
+        } catch (err) {
+            if (err instanceof InvalidCredentialsError) {
+                throw new UnauthorizedException(err.message);
+            }
+
+            throw err;
+        }
+    }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,103 @@
 import { Injectable } from '@nestjs/common';
+import { LoginDto } from './dto/login.dto';
+import { RegisterDto } from './dto';
+import { UserService } from '@user/user.service';
+import { UserEntity } from '@user/entities/user.entity';
+import { AccessToken, RefreshToken, Tokens } from './interfaces';
+import { InvalidCredentialsError, convertToMilliseconds } from '@common/utils';
+import { compare } from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { RedisService } from '@redis/redis.service';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        private readonly userService: UserService,
+        private readonly jwtService: JwtService,
+        private readonly redisService: RedisService,
+        private readonly configService: ConfigService,
+    ) {}
+
+    private getAccessToken(user: UserEntity): AccessToken {
+        return {
+            accessToken:
+                // TODO: Add Bearer to Constants
+                'Bearer ' +
+                this.jwtService.sign({
+                    id: user.id,
+                    email: user.email,
+                }),
+        };
+    }
+
+    private async getRefreshToken(userId: string, agent: string): Promise<RefreshToken> {
+        const expiresTime = convertToMilliseconds(this.configService.get('TOKEN_REFRESH_EXPIRE_TIME', '24h'));
+
+        const expiresDate = new Date(Date.now() + expiresTime);
+
+        const refreshToken = {
+            // TODO: add to constants
+            token: 'RT_' + userId + '_' + agent,
+            exp: expiresDate.toISOString(),
+            userId,
+            userAgent: agent,
+        };
+
+        await this.redisService.set(refreshToken.token, refreshToken, expiresTime);
+
+        return refreshToken;
+    }
+
+    async reg(registerDto: RegisterDto): Promise<UserEntity> {
+        return await this.userService.create(registerDto);
+    }
+
+    async login(loginDto: LoginDto, agent: string): Promise<Tokens> {
+        const user: UserEntity | null = await this.userService.findOneByEmail(loginDto.email);
+
+        if (!user) {
+            throw new InvalidCredentialsError();
+        }
+
+        if (!(await compare(loginDto.password, user.password))) {
+            throw new InvalidCredentialsError();
+        }
+
+        const accessToken = this.getAccessToken(user);
+
+        return {
+            accessToken: accessToken.accessToken,
+            refreshToken: await this.getRefreshToken(user.id, agent),
+        };
+    }
+
+    async refreshTokens(refreshToken: string, agent: string): Promise<Tokens> {
+        const token = await this.redisService.get<RefreshToken>(refreshToken);
+
+        if (!token) {
+            throw new InvalidCredentialsError();
+        }
+
+        await this.redisService.del(token.token);
+
+        if (new Date(token.exp) < new Date()) {
+            throw new InvalidCredentialsError();
+        }
+
+        const newRefreshToken = await this.getRefreshToken(token.userId, agent);
+
+        const user: UserEntity | null = await this.userService.findOneById(newRefreshToken.userId);
+
+        if (!user) {
+            throw new InvalidCredentialsError();
+        }
+
+        const accessToken = this.getAccessToken(user);
+
+        return {
+            accessToken: accessToken.accessToken,
+            refreshToken: newRefreshToken,
+        };
+    }
+}
