e43b2c59bfb9e5a44f6b8f8ff1a7eda00c446b96
fresemin <fresemin@gmail.com>
2024-03-24 17:11:58 +0300

    feat: auth guard

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,11 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserModule } from '@user/user.module';
 import { RedisModule } from '@redis/redis.module';
+import { GUARDS } from './guards';
 
 @Module({
     imports: [UserModule, RedisModule],
     controllers: [AuthController],
-    providers: [AuthService],
+    providers: [AuthService, ...GUARDS],
 })
 export class AuthModule {}
---
+++ src/auth/guards/index.ts
@@ -0,0 +1,3 @@
+import { AuthGuard } from './auth.guards';
+
+export const GUARDS = [AuthGuard];
