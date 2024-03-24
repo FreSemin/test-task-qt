36484ae3f2293cf044474bc5cc1d8b59474ad497
fresemin <fresemin@gmail.com>
2024-03-24 17:11:25 +0300

    ref: make jwt module global

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,27 @@
 import { DatabaseModule } from '@database/database.module';
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UserModule } from '@user/user.module';
 import { AuthModule } from '@auth/auth.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
     imports: [
         ConfigModule.forRoot({
             isGlobal: true,
             expandVariables: true,
+        }),
+        JwtModule.registerAsync({
+            global: true,
+            imports: [ConfigModule],
+            useFactory: async (configService: ConfigService) => ({
+                secret: configService.get('JWT_SECRET_KEY'),
+                signOptions: {
+                    // TODO: add default value
+                    expiresIn: configService.get('TOKEN_EXPIRE_TIME', '60s'),
+                },
+            }),
+            inject: [ConfigService],
         }),
         DatabaseModule,
         UserModule,
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,26 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserModule } from '@user/user.module';
-import { JwtModule } from '@nestjs/jwt';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 import { RedisModule } from '@redis/redis.module';
 
 @Module({
-    imports: [
-        UserModule,
-        JwtModule.registerAsync({
-            imports: [ConfigModule],
-            useFactory: async (configService: ConfigService) => ({
-                secret: configService.get('JWT_SECRET_KEY'),
-                signOptions: {
-                    // TODO: add default value
-                    expiresIn: configService.get('TOKEN_EXPIRE_TIME', '60s'),
-                },
-            }),
-            inject: [ConfigService],
-        }),
-        RedisModule,
-    ],
+    imports: [UserModule, RedisModule],
     controllers: [AuthController],
     providers: [AuthService],
 })
