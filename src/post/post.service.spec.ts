efa715482f29fb3d5cc5ae782371ed70a2bc6372
fresemin <fresemin@gmail.com>
2024-03-25 22:38:12 +0300

    test: post create

---
+++ src/post/post.service.spec.ts
@@ -0,0 +1,101 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { PostService } from './post.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { UserService } from '@user/user.service';
+import { RedisService } from '@redis/redis.service';
+import { CreatePostDto } from './dto';
+import { User } from '@user/entities/user.entity';
+import { Post } from './entity/post.entity';
+import { EntityNotFoundError } from '@common/utils';
+
+describe('PostService', () => {
+    let service: PostService;
+    let postRepository: Repository<Post>;
+
+    const mockPostRepository = {
+        create: jest.fn(),
+        save: jest.fn(),
+    };
+
+    const mockUserService = {
+        findOneById: jest.fn((x) => x),
+    };
+
+    const mockRedisService = {};
+
+    const authorMock: User = {
+        id: 'f2e1593c-bf97-4421-bd07-375440610dd2',
+        name: 'user1',
+        email: 'email1@gmail.com',
+        password: '12345678',
+        posts: [],
+    };
+
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [
+                PostService,
+                {
+                    provide: getRepositoryToken(Post),
+                    useValue: mockPostRepository,
+                },
+                {
+                    provide: UserService,
+                    useValue: mockUserService,
+                },
+                {
+                    provide: RedisService,
+                    useValue: mockRedisService,
+                },
+            ],
+        }).compile();
+
+        service = module.get<PostService>(PostService);
+        postRepository = module.get<Repository<Post>>(getRepositoryToken(Post));
+    });
+
+    it('should be defined', () => {
+        expect(service).toBeDefined();
+    });
+
+    describe('create', () => {
+        it('should create new post successfully', async () => {
+            const createPostDto: CreatePostDto = {
+                name: 'post name',
+                description: 'post description',
+            };
+
+            const post: Post = {
+                id: '42ca922b-a6cb-476c-9152-ac901622949f',
+                name: 'post name',
+                description: 'post description',
+                createdAt: new Date('2024-03-25T15:26:20.209Z'),
+                authorId: authorMock.id,
+                author: {
+                    ...authorMock,
+                },
+            };
+
+            jest.spyOn(postRepository, 'save').mockResolvedValueOnce(post);
+
+            const result = await service.create(createPostDto, authorMock.id);
+
+            expect(result).toEqual(post);
+            expect(result.authorId).toEqual(authorMock.id);
+        });
+
+        it('should throw error if author is not exist', async () => {
+            const createPostDto: CreatePostDto = {
+                name: 'post name',
+                description: 'post description',
+            };
+
+            jest.spyOn(mockUserService, 'findOneById').mockResolvedValueOnce(null);
+
+            await expect(async () => {
+                await service.create(createPostDto, authorMock.id);
+            }).rejects.toThrow(EntityNotFoundError);
+        });
+    });
+});
